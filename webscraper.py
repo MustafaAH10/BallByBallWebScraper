298a6ffa00c1a204d849bf7d91b2483631f41b02
MustafaAH10 <56750694+MustafaAH10@users.noreply.github.com>
2022-11-15 23:31:55 +0800

    Create webscraper.py

---
+++ webscraper.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get('https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2022-23-1298134/england-vs-india-2nd-semi-final-1298178/ball-by-ball-commentary').text
+#print(html_text)
+
+soup = BeautifulSoup(html_text, 'lxml')
+bbbs = soup.find_all('div', class_ = 'lg:hover:ds-bg-ui-fill-translucent ds-hover-parent ds-relative')
+#print(byb)
+
+for ball in bbbs:
+    bowlertobatter = ball.find('div', class_ = 'ds-leading-none ds-mb-0.5').text
+    print(bowlertobatter)
